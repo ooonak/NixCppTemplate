49a9bbf56fb3fdecad8597229cee2b959d1b7f5c
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2023-12-11 20:36:08 +0100

    Simple approach, no flake-utils or flake-parts.

--- flake.nix
+++ flake.nix
@@ -3,46 +3,20 @@
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
   outputs = { self, nixpkgs }:
 
-  let
-    allSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-    
-    forAllSystems = fn: nixpkgs.lib.genAttrs allSystems 
-      ( system: 
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+    let
+      #allSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      allSystems = [ "x86_64-linux" ];
 
-          nativeBuildInputs = with pkgs; [
-            cmake
-            clang-tools_16
-            gtest
-            #lldb_16
-            mold
-            ninja
-            pkg-config
-            #valgrind
-          ];
+      forAllSystems = func: nixpkgs.lib.genAttrs allSystems
+        (system: func nixpkgs.legacyPackages.${system});
 
-          buildInputs = with pkgs; [
-            spdlog
-          ];
-      in
-      with pkgs;
-      {
-        #devShells.default = forAllSystems ( { pkgs } : {
-          devShells.default = pkgs.mkShell rec {
-            name = "NixCppTempl";
-            inherit buildInputs nativeBuildInputs;
+    in
+    {
 
-            KEY = "value";
-            shellHook = ''
-              export PS1="${name}"
-            '';
-          }; # devShell 
-        #}); # devShells
-        }
-      );
-    
+      packages = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./package.nix { };
+      });
+
+    };
 }
 
