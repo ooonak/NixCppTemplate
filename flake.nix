0cca9256d1fc7f440798b37a6718de1e9c5c1ba9
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2023-12-11 22:02:49 +0100

    Docker does copy artifacts into container after flake-parts is removed.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Nix C++ template";
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, ... }:
 
     let
       #allSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
@@ -15,6 +15,38 @@
 
       packages = forAllSystems (pkgs: {
         default = pkgs.callPackage ./package.nix { };
+        clang = pkgs.callPackage ./package.nix { stdenv = pkgs.clang16Stdenv; };
+        gcc = pkgs.callPackage ./package.nix { stdenv = pkgs.gccStdenv; };
+
+        dockerImage = pkgs.dockerTools.buildImage {
+          name = "CppTemplAppContainer";
+          created = "now";
+          tag = builtins.substring 0 9 (self.rev or "dev");
+          copyToRoot = pkgs.buildEnv {
+            paths = with pkgs; [
+              #self.packages.default
+              #binSh
+              #fakeNss
+              #cacert
+              #coreutils
+              #bash
+            ];
+            name = "cpptemplapp-root";
+            pathsToLink = [ "/bin" "/lib" "/include" ];
+          };
+          config = {
+            Cmd = [ "bin/CppTemplApp" ];
+            Env = [
+              #"SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+              #"SYSTEM_CERTIFICATE_PATH=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+            ];
+            #User = "nobody";
+            #ExposedPorts = { "8000/tcp" = {}; };
+            #WorkingDir = "/data";
+            #Volumes = { "/data" = { }; };
+          };
+        };
+
       });
 
     };
