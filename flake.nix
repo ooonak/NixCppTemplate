6c6f7ac0921e3bf33aa08fac3e628dfa9f79593f
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2023-12-11 22:08:53 +0100

    Removed Docker part.

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
   description = "Nix C++ template";
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs }:
 
     let
-      #allSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       allSystems = [ "x86_64-linux" ];
 
       forAllSystems = func: nixpkgs.lib.genAttrs allSystems
@@ -18,34 +17,6 @@
         clang = pkgs.callPackage ./package.nix { stdenv = pkgs.clang16Stdenv; };
         gcc = pkgs.callPackage ./package.nix { stdenv = pkgs.gccStdenv; };
 
-        dockerImage = pkgs.dockerTools.buildImage {
-          name = "CppTemplAppContainer";
-          created = "now";
-          tag = builtins.substring 0 9 (self.rev or "dev");
-          copyToRoot = pkgs.buildEnv {
-            paths = with pkgs; [
-              #self.packages.default
-              #binSh
-              #fakeNss
-              #cacert
-              #coreutils
-              #bash
-            ];
-            name = "cpptemplapp-root";
-            pathsToLink = [ "/bin" "/lib" "/include" ];
-          };
-          config = {
-            Cmd = [ "bin/CppTemplApp" ];
-            Env = [
-              #"SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-              #"SYSTEM_CERTIFICATE_PATH=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-            ];
-            #User = "nobody";
-            #ExposedPorts = { "8000/tcp" = {}; };
-            #WorkingDir = "/data";
-            #Volumes = { "/data" = { }; };
-          };
-        };
 
       });
 
